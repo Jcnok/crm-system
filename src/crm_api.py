f3841f8e87531ac5217c2df9058126e1bf509055
Julio Okuda <julio.okuda@gmail.com>
2024-10-03 20:42:27 -0300

    add: streamlit examples

---
+++ src/crm_api.py
@@ -0,0 +1,213 @@
+from fastapi import FastAPI, HTTPException
+import pandas as pd
+import sqlalchemy
+from dotenv import load_dotenv
+import os
+from sqlalchemy import text, select
+
+load_dotenv()  # Carrega as variáveis do arquivo .env
+
+# Configuração do banco de dados
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASS")
+
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:5432/{DB_NAME}"  # Monta a URL da conexão
+
+engine = sqlalchemy.create_engine(DATABASE_URL)
+
+app = FastAPI()
+
+# Endpoint para obter o Faturamento Total
+@app.get("/total_revenue")
+async def get_total_revenue():
+    with engine.connect() as conn:
+        stmt = text("SELECT total_revenue FROM total_revenue")  # Usa a função text() para executar a consulta
+        result = conn.execute(stmt).fetchone()
+        if result:
+            return {"total_revenue": result[0]}
+        else:
+            raise HTTPException(status_code=404, detail="KPI não encontrada")
+
+# Endpoint para obter o Número Total de Vendas
+@app.get("/total_sales")
+async def get_total_sales():
+    with engine.connect() as conn:
+        stmt = text("SELECT total_sales FROM  total_sales") 
+        result = conn.execute(stmt).fetchone()
+        if result:
+            return {"total_sales": result[0]}
+        else:
+            raise HTTPException(status_code=404, detail="KPI não encontrada")
+
+# Endpoint para obter o Valor Médio da Venda
+@app.get("/average_sale_value")
+async def get_average_sale_value():
+    with engine.connect() as conn:
+        stmt = text("SELECT average_sale_value FROM  average_sale_value") 
+        result = conn.execute(stmt).fetchone()
+        if result:
+            return {"average_sale_value": result[0]}
+        else:
+            raise HTTPException(status_code=404, detail="KPI não encontrada")
+
+# Endpoint para obter a Quantidade Média de Produtos por Venda
+@app.get("/average_products_per_sale")
+async def get_average_products_per_sale():
+    with engine.connect() as conn:
+        stmt = text("SELECT average_products_per_sale FROM  average_products_per_sale") 
+        result = conn.execute(stmt).fetchone()
+        if result:
+            return {"average_products_per_sale": result[0]}
+        else:
+            raise HTTPException(status_code=404, detail="KPI não encontrada")
+
+# Endpoint para obter o Ticket Médio
+@app.get("/average_ticket")
+async def get_average_ticket():
+    with engine.connect() as conn:
+        stmt = text("SELECT average_ticket FROM  average_ticket")
+        result = conn.execute(stmt).fetchone()
+        if result:
+            return {"average_ticket": result[0]}
+        else:
+            raise HTTPException(status_code=404, detail="KPI não encontrada")
+
+# Endpoint para obter o Produto Mais Vendido (em Valor)
+@app.get("/best_selling_product_value")
+async def get_best_selling_product_value():
+    with engine.connect() as conn:
+        stmt = text("SELECT * FROM  best_selling_product_value")
+        result = conn.execute(stmt).fetchone()
+        if result:
+            return {"produto": result[0], "total_product_revenue": result[1]}
+        else:
+            raise HTTPException(status_code=404, detail="KPI não encontrada")
+
+# Endpoint para obter o Produto Mais Vendido (em Quantidade)
+@app.get("/best_selling_product_quantity")
+async def get_best_selling_product_quantity():
+    with engine.connect() as conn:
+        stmt = text("SELECT * FROM  best_selling_product_quantity")
+        result = conn.execute(stmt).fetchone()
+        if result:
+            return {"produto": result[0], "total_product_quantity": result[1]}
+        else:
+            raise HTTPException(status_code=404, detail="KPI não encontrada")
+
+# Endpoint para obter a Receita por Produto
+@app.get("/product_revenue")
+async def get_product_revenue():
+    with engine.connect() as conn:
+        results = conn.execute(text("SELECT * FROM  product_revenue")).fetchall()
+        if results:
+            return [{"produto": row[0], "product_revenue": row[1]} for row in results]
+        else:
+            raise HTTPException(status_code=404, detail="KPI não encontrada")
+
+# Endpoint para obter o Vendedor com Mais Vendas (em Valor)
+@app.get("/top_salesperson_value")
+async def get_top_salesperson_value():
+    with engine.connect() as conn:
+        stmt = text("SELECT * FROM  top_salesperson_value")
+        result = conn.execute(stmt).fetchone()
+        if result:
+            return {"email": result[0], "salesperson_total_revenue": result[1]}
+        else:
+            raise HTTPException(status_code=404, detail="KPI não encontrada")
+
+# Endpoint para obter o Vendedor com Mais Vendas (em Quantidade)
+@app.get("/top_salesperson_quantity")
+async def get_top_salesperson_quantity():
+    with engine.connect() as conn:
+        stmt = text("SELECT * FROM  top_salesperson_quantity")
+        result = conn.execute(stmt).fetchone()
+        if result:
+            return {"email": result[0], "salesperson_total_sales": result[1]}
+        else:
+            raise HTTPException(status_code=404, detail="KPI não encontrada")
+
+# Endpoint para obter o Número de Vendas por Vendedor
+@app.get("/sales_per_salesperson")
+async def get_sales_per_salesperson():
+    with engine.connect() as conn:
+        results = conn.execute(text("SELECT * FROM  sales_per_salesperson")).fetchall()
+        if results:
+            return [{"email": row[0], "sales_per_salesperson": row[1]} for row in results]
+        else:
+            raise HTTPException(status_code=404, detail="KPI não encontrada")
+
+# Endpoint para obter o Faturamento por Vendedor
+@app.get("/revenue_per_salesperson")
+async def get_revenue_per_salesperson():
+    with engine.connect() as conn:
+        results = conn.execute(text("SELECT * FROM  revenue_per_salesperson")).fetchall()
+        if results:
+            return [{"email": row[0], "revenue_per_salesperson": row[1]} for row in results]
+        else:
+            raise HTTPException(status_code=404, detail="KPI não encontrada")
+
+# Endpoint para obter as Vendas por Dia
+@app.get("/sales_per_day")
+async def get_sales_per_day():
+    with engine.connect() as conn:
+        results = conn.execute(text("SELECT * FROM  sales_per_day")).fetchall()
+        if results:
+            return [{"sales_date": str(row[0]), "sales_per_day": row[1]} for row in results]
+        else:
+            raise HTTPException(status_code=404, detail="KPI não encontrada")
+
+# Endpoint para obter as Vendas por Mês
+@app.get("/sales_per_month")
+async def get_sales_per_month():
+    with engine.connect() as conn:
+        results = conn.execute(text("SELECT * FROM  sales_per_month")).fetchall()
+        if results:
+            return [{"sales_month": row[0], "sales_per_month": row[1]} for row in results]
+        else:
+            raise HTTPException(status_code=404, detail="KPI não encontrada")
+
+# Endpoint para obter as Vendas por Ano
+@app.get("/sales_per_year")
+async def get_sales_per_year():
+    with engine.connect() as conn:
+        results = conn.execute(text("SELECT * FROM  sales_per_year")).fetchall()
+        if results:
+            return [{"sales_year": row[0], "sales_per_year": row[1]} for row in results]
+        else:
+            raise HTTPException(status_code=404, detail="KPI não encontrada")
+
+# Endpoint para obter o Faturamento por Dia
+@app.get("/revenue_per_day")
+async def get_revenue_per_day():
+    with engine.connect() as conn:
+        results = conn.execute(text("SELECT * FROM  revenue_per_day")).fetchall()
+        if results:
+            return [{"revenue_date": str(row[0]), "revenue_per_day": row[1]} for row in results]
+        else:
+            raise HTTPException(status_code=404, detail="KPI não encontrada")
+
+# Endpoint para obter o Faturamento por Mês
+@app.get("/revenue_per_month")
+async def get_revenue_per_month():
+    with engine.connect() as conn:
+        results = conn.execute(text("SELECT * FROM  revenue_per_month")).fetchall()
+        if results:
+            return [{"revenue_month": row[0], "revenue_per_month": row[1]} for row in results]
+        else:
+            raise HTTPException(status_code=404, detail="KPI não encontrada")
+
+# Endpoint para obter o Faturamento por Ano
+@app.get("/revenue_per_year")
+async def get_revenue_per_year():
+    with engine.connect() as conn:
+        results = conn.execute(text("SELECT * FROM  revenue_per_year")).fetchall()
+        if results:
+            return [{"revenue_year": row[0], "revenue_per_year": row[1]} for row in results]
+        else:
+            raise HTTPException(status_code=404, detail="KPI não encontrada")
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("crm_api:app", host="0.0.0.0", port=5000, reload=True)
---
+++ src/dash2.py
@@ -0,0 +1,186 @@
+import streamlit as st
+import plotly.express as px
+import pandas as pd
+from datetime import datetime
+from pydantic import ValidationError
+
+from contract import Produto, Vendas
+from database import (
+    delete_all_sales_data,
+    salvar_no_postgres,
+    salvar_no_postgres_em_lote,
+    obter_dados_api
+)
+
+# Configuração da página
+st.set_page_config(page_title="CRM System", layout="wide")
+
+# Função para renderizar o formulário de entrada de dados
+def render_data_entry():
+    st.title("CRM System")
+    st.write("Este é um sistema de CRM construído com Streamlit.")
+
+    # Cria os campos de entrada para os dados da venda
+    email = st.text_input("Email do vendedor")
+    data = st.date_input("Data em que a venda foi realizada", format="DD/MM/YYYY")
+    hora = st.time_input("Hora em que a venda foi realizada")
+    valor = st.number_input("Valor da venda")
+    quantidade = st.number_input("Quantidade de produtos vendidos")
+    produto = st.selectbox("Escolha o produto vendido", [p.value for p in Produto])
+
+    # Botão para salvar os dados da venda
+    if st.button("Salvar"):
+        try:
+            data_hora = datetime.combine(data, hora)
+            venda = Vendas(
+                email=email,
+                data=data_hora,
+                valor=valor,
+                quantidade=quantidade,
+                produto=Produto(produto),
+            )
+            salvar_no_postgres(venda)
+            st.success("Venda salva com sucesso!")
+            st.write(venda)
+        except ValidationError as e:
+            st.error(f"Erro de validação: {e}")
+
+    # Adiciona a funcionalidade de importar dados de arquivo CSV
+    st.subheader("Importar dados de vendas (arquivo CSV)")
+    st.markdown(
+        """Copie o link e cole em 'Browse files' para usar o arquivo de exemplo:
+           https://raw.githubusercontent.com/Jcnok/crm-system/refs/heads/master/data/sales_data.csv
+        """
+    )
+    st.write("O arquivo CSV deve estar no seguinte formato:")
+    st.markdown(
+        """
+        - Colunas: email, data, valor, quantidade, produto
+        - Formato da data: AAAA-MM-DD HH:MM:SS
+        - Formato do valor: número decimal com duas casas decimais (ex: 10.50)
+        - Formato da quantidade: número inteiro
+        - Produto: um dos valores definidos na enum Produto (Produto 1, Produto 2, Produto 3)
+        """
+    )
+    uploaded_file = st.file_uploader("Escolha um arquivo CSV", type=["csv"])
+    if uploaded_file is not None:
+        try:
+            # Carrega os dados do arquivo CSV
+            sales_data = pd.read_csv(uploaded_file)
+
+            # Converte as colunas para o formato esperado
+            sales_data["data"] = pd.to_datetime(sales_data["data"])
+            sales_data["valor"] = sales_data["valor"].astype(float)
+            sales_data["quantidade"] = sales_data["quantidade"].astype(int)
+            sales_data["produto"] = sales_data["produto"].apply(lambda x: Produto(x))
+
+            # Cria uma lista de objetos Vendas a partir dos dados do CSV
+            vendas = [
+                Vendas(
+                    email=row["email"],
+                    data=row["data"],
+                    valor=row["valor"],
+                    quantidade=row["quantidade"],
+                    produto=row["produto"],
+                )
+                for _, row in sales_data.iterrows()
+            ]
+
+            # Salva os dados em lote no banco de dados
+            if salvar_no_postgres_em_lote(vendas):
+                st.success("Dados importados do CSV com sucesso!")
+            else:
+                st.error("Erro ao importar dados do CSV.")
+        except Exception as e:
+            st.error(f"Erro ao importar dados do CSV: {e}")
+
+    
+
+#Função para apagar todo o banco de dados
+def del_database():    
+    # Botão para deletar todos os dados do banco de dados
+    st.subheader("Apagar todos os dados do banco de dados! (Botão do Pânico 🚨)")
+    if st.button("Deletar todos os dados"):
+        if delete_all_sales_data():
+            st.success("Todos os dados foram deletados do banco de dados.", icon="🔥")
+        else:
+            st.error("Erro ao deletar os dados do banco de dados.")
+
+# Função para renderizar o dashboard
+def render_dashboard():
+    st.title("CRM System Dashboard")
+
+    # Visão Geral
+    st.header("Visão Geral das Vendas")
+    col1, col2, col3 = st.columns(3)
+    
+    total_revenue = obter_dados_api("total_revenue")    
+    with col1:
+        st.metric("Faturamento Total", value=f"R$ {total_revenue['total_revenue']:.2f}")
+
+    total_sales = obter_dados_api("total_sales")    
+    with col2:
+        st.metric("Número Total de Vendas", value=f"{total_sales['total_sales']}")
+    
+    average_ticket = obter_dados_api("average_ticket")    
+    with col3:
+        st.metric("Ticket Médio", value=f"R$ {average_ticket['average_ticket']:.2f}")
+    
+    # Botão para atualizar os dados
+    if st.button("Atualizar Dados"):
+      st.rerun()  # Reinicia a execução do dashboard
+
+    # Análise de Produtos
+    st.header("Análise de Produtos")
+    product_revenue = obter_dados_api("product_revenue")
+    product_revenue_df = pd.DataFrame(product_revenue)
+    fig_pie = px.pie(
+        product_revenue_df, 
+        values="product_revenue", 
+        names="produto",
+        title="Participação dos Produtos na Receita",
+        color_discrete_sequence=px.colors.qualitative.Pastel
+    )
+    st.plotly_chart(fig_pie, use_container_width=True)
+
+    # Desempenho de Vendedores
+    st.header("Desempenho de Vendedores")
+    revenue_per_salesperson = obter_dados_api("revenue_per_salesperson")
+    revenue_per_salesperson_df = pd.DataFrame(revenue_per_salesperson)
+    fig_bar = px.bar(
+        revenue_per_salesperson_df,
+        x="email",
+        y="revenue_per_salesperson",
+        title="Faturamento por Vendedor",
+        labels={"email": "Vendedor", "revenue_per_salesperson": "Faturamento"}
+    )
+    st.plotly_chart(fig_bar, use_container_width=True)
+
+    # Tendências Temporais
+    st.header("Tendências Temporais")
+    revenue_per_month = obter_dados_api("revenue_per_month")
+    revenue_per_month_df = pd.DataFrame(revenue_per_month)
+    fig_line = px.line(
+        revenue_per_month_df,
+        x="revenue_month",
+        y="revenue_per_month",
+        title="Evolução do Faturamento Mensal",
+        labels={"revenue_month": "Mês", "revenue_per_month": "Faturamento"}
+    )
+    st.plotly_chart(fig_line, use_container_width=True)
+
+    
+# Função principal
+def main():
+    st.sidebar.title("Navegação")
+    page = st.sidebar.radio("Ir para", ["Entrada de Dados", "Dashboard", "Apagar Dados"])
+
+    if page == "Entrada de Dados":
+        render_data_entry()
+    elif page == "Dashboard":
+        render_dashboard()
+    elif page == "Apagar Dados":
+        del_database()
+    
+if __name__ == "__main__":
+    main()
