5500ad0eefeb92eaacc018f5b78c66be7906d838
Julio Okuda <julio.okuda@gmail.com>
2024-10-10 19:02:30 -0300

    fix: correção da gold

--- vendas_dbt/models/gold/revenue_per_month.sql
+++ vendas_dbt/models/gold/revenue_per_month.sql
@@ -1,14 +1,15 @@
 {{ config(materialized='view') }}
 
 -- KPI: Faturamento por Mês
--- Calcula o faturamento total gerado em cada mês.
+-- Calcula o faturamento total gerado em cada mês por ano.
 
 SELECT
+    EXTRACT(YEAR FROM data) AS revenue_year,
     EXTRACT(MONTH FROM data) AS revenue_month,
     SUM(valor) AS revenue_per_month
 FROM
     {{ ref('silver_vendas') }}
 GROUP BY
-    revenue_month
+    revenue_year, revenue_month
 ORDER BY
-    revenue_month ASC
+    revenue_year ASC, revenue_month ASC
